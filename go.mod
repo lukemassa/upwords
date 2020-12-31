73adeffe502dc5173c48b328bbe5600983825bc1
Luke Massa <lmassa@tripadvisor.com>
2020-12-31 14:26:13 -0500

    Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lukemassa/upwards
+
+go 1.15
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-import "fmt"
+import "github.com/lukemassa/upwords/pkg/game"
 
+func main() {
+	player1 := game.Player("Luke")
+	player2 := game.Player("Nick")
 
+	g := game.New(&player1, &player2)
+
+	g.Play()
+}
